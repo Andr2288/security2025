fa98951ea602887268bee71af6cba15fe1994e5b
chorn <andriychornookyi228@gmail.com>
2025-10-08 20:02:21 +0300

    Task №4. CSRF

--- src/main/java/edu/pzks/security2025/config/SecurityConfig.java
+++ src/main/java/edu/pzks/security2025/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,7 +23,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> csrf.disable()).
+        http.csrf(AbstractHttpConfigurer::disable).
                 authorizeHttpRequests(auth -> auth.anyRequest().authenticated()).
                 httpBasic(Customizer.withDefaults());
 
